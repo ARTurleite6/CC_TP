9d233eb549bdb3072831827119ef4e4c80068058
Artur Leite <arturleite164@gmail.com>
2022-11-24 19:50:40 +0000

    timeout on transfer zone

--- server_module/tcp.py
+++ server_module/tcp.py
@@ -1,6 +1,6 @@
 import socket
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import time, sleep
 from server_module.serverconfig import ServerConfig
 from server_module.database import Origin
@@ -17,7 +17,6 @@ class TCPZoneTransferSender(Thread):
         self.domain = domain
 
     def run(self):
-        begin = datetime.now()
         # msg = "Vamos la comecar chavalo"
         # self.server_socket.sendall(msg.encode('utf-8'))
         with open(self.db_file) as file:
@@ -35,6 +34,7 @@ class TCPZoneTransferSender(Thread):
                         self.server_socket.sendall((message_content.encode('utf-8')))
                         number_bytes += len(message_content)
                         counter += 1
+                        self.server_config.log_info(self.domain, f"{datetime.now()} ZT {self.server_socket.gethostname()} SP")
 
         self.server_socket.close()
 
@@ -80,56 +80,22 @@ def transfer_zone_receive(server_ip: str, port: int, domain: str, server_config:
             receiver.sendall(message)
             line_counter = 0
             lines_db = []
-            try:
-                while line_counter < number_lines:
-                    lines = receiver.recv(1024)
-                    if lines:
-                        lines = lines.decode('utf-8').splitlines()
-                        for line in lines:
-                            line_camps = line.split(sep= ' ', maxsplit=1)
-                            line_number = int(line_camps[0])
-                            if line_counter == line_number:
-                                lines_db.append(line_camps[1])
-                                line_counter += 1
+            wait_time = datetime.now() + timedelta(seconds=TTL_TRANSFER)
+            break_loop = False
+            while not break_loop and line_counter < number_lines:
+                lines = receiver.recv(1024)
+                if lines:
+                    lines = lines.decode('utf-8').splitlines()
+                    for line in lines:
+                        line_camps = line.split(sep= ' ', maxsplit=1)
+                        line_number = int(line_camps[0])
+                        if line_counter == line_number:
+                            lines_db.append(line_camps[1])
+                            line_counter += 1
+                if wait_time < datetime.now():
+                    break_loop = True
 
-                if line_counter == number_lines:
-                    server_config.add_database_entries_file(lines_db, Origin.SP, domain) 
-                    return True
-            except TimeoutError:
-                return False 
+            if line_counter == number_lines:
+                server_config.add_database_entries_file(lines_db, Origin.SP, domain) 
+                return True
         return False
-
-# class TCPZoneTransferReceiver():
-#     def __init__(self, server_ip: str, port: int, domain: str, server_config: ServerConfig):
-#         self.domain = domain
-#         self.port = port
-#         self.server_ip = server_ip
-#         self.server_config = server_config
-#
-#     def run(self):
-#         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as receiver:
-#             receiver.connect((self.server_ip, self.port))
-#             msg = self.domain
-#             receiver.sendall(msg.encode('utf-8'))
-#             message = receiver.recv(10)
-#             if message:
-#                 message_dec = message.decode('utf-8')
-#                 number_lines = int(message_dec)
-#                 receiver.sendall(message)
-#
-#                 line_counter = 0
-#                 lines_db = []
-#                 while line_counter < number_lines:
-#                     lines = receiver.recv(1024)
-#                     if lines:
-#                         lines = lines.decode('utf-8').splitlines()
-#                         for line in lines:
-#                             line_camps = line.split(sep= ' ', maxsplit=1)
-#                             line_number = int(line_camps[0])
-#                             if line_counter == line_number:
-#                                 lines_db.append(line_camps[1])
-#                                 line_counter += 1
-#
-#                 self.server_config.add_database_entries_file(lines_db, Origin.SP, self.domain) 
-#             
-#
